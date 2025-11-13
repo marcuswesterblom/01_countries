3fbe670f267164cc42928ad6b821249b2a14d147
Marcus <marcus@Larss-MBP-2.lan>
2025-11-13 15:15:59 +0100

    Styling

--- src/utils/createHtmlUtils.ts
+++ src/utils/createHtmlUtils.ts
@@ -9,52 +9,42 @@ export const createHtml = (countries: Country[]) => {
         const img = document.createElement("img");
         const countryName = document.createElement("h2");
         const countryNameOfficial = document.createElement("h3");
+        const noInfo = document.createElement("div");
+        const noInfoText = document.createElement("p");
+
+        const infoFlag = document.createElement("p");
+        infoFlag.className = "infoFlag";
+        infoFlag.innerHTML = country.flags.alt ?? country.name.common;
+        countryContainer.appendChild(infoFlag);
 
         countryContainer.className = "countryContainer";
         imgContainer.className = "imgContainer";
         countryName.className = "countryName";
         countryNameOfficial.className = "countryNameOfficial";
+        noInfo.className = "noInfo";
 
         img.src = country.flags.svg;
         img.alt = country.flags.alt ?? country.name.common;
         countryName.innerHTML = country.name.common;
         countryNameOfficial.innerHTML = country.name.official;
+        noInfoText.textContent = `No information available about the flag of ${countryName.innerHTML}`;
 
         countryContainer.appendChild(imgContainer);
         imgContainer.appendChild(img);
         countryContainer.appendChild(countryName);
         countryContainer.appendChild(countryNameOfficial);
         app?.appendChild(countryContainer);
+        noInfo.appendChild(noInfoText);
+        countryContainer.appendChild(noInfo);
 
         countryContainer.addEventListener("click", () => {
-            const existingInfo = countryContainer.querySelector(".infoFlag");
-            const existingNoInfo = countryContainer.querySelector(".noInfo");
-            if(existingInfo) {
-                existingInfo.remove();
 
-            } else {
-                const infoFlag = document.createElement("p");
-                infoFlag.className = "infoFlag";
-                infoFlag.innerHTML = country.flags.alt ?? country.name.common;
-                countryContainer.appendChild(infoFlag);
+            infoFlag.classList.toggle("show");
+
+            if (img.alt === "") {
+            noInfo.classList.toggle("show");
             }
-            if(existingNoInfo) {
-                existingNoInfo.remove();
-
-            } else {
-            if(img.alt === "") {
-            const noInfo = document.createElement("div");
-            const noInfoText = document.createElement("p");
-
-            noInfo.className = "noInfo";
-
-            noInfoText.textContent = `No information available about the flag of ${countryName.innerHTML}`;
-
-            noInfo.appendChild(noInfoText);
-            countryContainer.appendChild(noInfo);
-            }
-        }
+        
         });
     });
-
 }
